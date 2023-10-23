fb974e47f47acccd959dec761938254f061f470f
mrpanzerr <147166488+mrpanzerr@users.noreply.github.com>
2023-10-23 13:55:37 -0400

    Update Automobile.java

--- Lab6Transportation/src/Automobile.java
+++ Lab6Transportation/src/Automobile.java
@@ -15,17 +15,23 @@ public class Automobile extends LandTransportation{
     public Automobile(String name, double cost, boolean purchaseType,
                       int averageSpeed, int numOfPassengers, String departure, String color,
                       int wheels, String infrastructure, String steer, String make, String model, boolean stick) {
+        // From LandTransportation
         super(name, cost, purchaseType, averageSpeed, numOfPassengers,
                 departure, color, wheels, infrastructure, steer);
+
         this.make = make; this.model = model; this.stick = stick;
     }
 
     // No Argument Constructor
     public Automobile() {
+        // From Transportation
         this.setName("Automobile"); this.setCost(79.44); this.setPurchaseType(false);
-        this.setAverageSpeed(60); this.setNumOfPassengers(5); this.setDeparture("10AM"); this.setWheels(4);
-        this.setInfrastructure("Highway"); this.setColor("Black"); this.setSteer("Wheel");
-        this.make = "Sonata"; this.model = "Sedan"; this.stick = false;
+        this.setAverageSpeed(60); this.setNumOfPassengers(5); this.setDeparture("10AM"); this.setColor("Black");
+
+        // From LandTransportation
+        this.setWheels(4); this.setInfrastructure("Highway"); this.setSteer("Wheel");
+
+        this.make = "Hyundai"; this.model = "Sonata"; this.stick = false;
     }
 
     // Accessor Methods
@@ -43,9 +49,10 @@ public class Automobile extends LandTransportation{
      * overridden toString method
      * @return a string
      */
+    // From LandTransportation
     @Override
     public String toString() {
-        return super.toString() + String.format("%21s: %s\n%21s: %s\n%21s: %s\n",
+        return super.toString() + String.format("%21s: %s\n%21s: %s\n%21s: %s\n\n",
                 "Make", getMake(),
                 "Model", getModel(),
                 "Stick or Automatic", getStick() ? "Stick" : "Automatic");
@@ -55,7 +62,13 @@ public class Automobile extends LandTransportation{
      * display travel instructions
      * @return a string
      */
+    // From Transportation
+    @Override
     public String travelInstructions() {
-        return "";
+        return "You can pick up your " + getColor() + " " +
+                getMake() + " " + getModel() + " at " +
+                getDeparture() + " at Mitsubishi Rentals.\nAll " +
+                "rentals must be returned to Mitsubishi Rentals within " +
+                "72 hours of your pickup time.";
     }
 }
